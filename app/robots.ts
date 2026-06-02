36bf25b613645c539ac51775288fdf45c537f738
imranbanglaweb <md.imran1200@gmail.com>
2026-06-02 11:25:08 +0600

    23-05-2026

--- app/robots.ts
+++ app/robots.ts
@@ -16,6 +16,7 @@ export default function robots(): MetadataRoute.Robots {
            '/contact',
            '/blog',
            '/api/search',
+           '/sitemap.xml',
          ],
          disallow: [
            '/admin/',
